ddf23900beda81ac2e147476b6809904ee251c9c
sneha-chauhan <gk1720@myamu.ac.in>
2021-12-30 10:57:53 +0530

    Added a splash screen and changed the UI's

--- app/src/main/java/com/example/medicalapp/MainActivity.kt
+++ app/src/main/java/com/example/medicalapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.medicalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button=findViewById(R.id.get_otp)
+        button.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
