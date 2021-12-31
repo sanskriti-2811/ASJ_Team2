74f1b68659499be7167397fbb8afb1f4da2512e5
sneha-chauhan <gk1720@myamu.ac.in>
2021-12-31 17:57:44 +0530

    Change the background Added Wards page

--- app/src/main/java/com/example/medicalapp/LoginActivity.kt
+++ app/src/main/java/com/example/medicalapp/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.medicalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val button: Button =findViewById(R.id.login)
+        button.setOnClickListener{
+            val intent = Intent(this, WardsPage::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/medicalapp/WardsPage.kt
@@ -0,0 +1,51 @@
+package com.example.medicalapp
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class WardsPage : AppCompatActivity() {
+    lateinit var listView: ListView
+    var wards: ArrayList<String> = ArrayList()
+    var arrayAdapter: ArrayAdapter<String>? = null
+    lateinit var etSearch: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wards_page)
+
+        title = "KotlinApp"
+        listView = findViewById(R.id.listView)
+        etSearch = findViewById(R.id.etSearch)
+        wards.add("COVID ICU 1")
+        wards.add("COVID ICU ")
+        wards.add("COVID ICU 10")
+        wards.add("COVID ICU 100")
+        wards.add("COVID ICU 101")
+        wards.add("COVID ICU 102")
+        wards.add("COVID ICU 103")
+        wards.add("COVID ICU 104")
+        wards.add("COVID ICU 105")
+        wards.add("COVID ICU 106")
+        wards.add("COVID ICU 107")
+        wards.add("COVID ICU 108")
+        wards.add("COVID ICU 109")
+        wards.add("COVID ICU 11")
+
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.activity_list_item, android.R.id.text1, wards)
+        listView.adapter = arrayAdapter
+        etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                arrayAdapter!!.filter.filter(s)
+            }
+            override fun afterTextChanged(s: Editable) {}
+        })
+    }
+}
