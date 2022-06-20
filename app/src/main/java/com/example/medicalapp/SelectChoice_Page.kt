b511e6dce44268a85ccff5f29f2c20a4035ad221
sneha-chauhan <gk1720@myamu.ac.in>
2022-06-20 19:14:02 +0530

    Linked The Biomedical and Hand Hygiene component

--- app/src/main/java/com/example/medicalapp/SelectChoice_Page.kt
+++ app/src/main/java/com/example/medicalapp/SelectChoice_Page.kt
@@ -2,6 +2,8 @@ package com.example.medicalapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
@@ -29,6 +31,16 @@ class SelectChoice_Page : AppCompatActivity() {
         }
 
 
+        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
+        card1.startAnimation(sideAnimation)
+        Handler().postDelayed({
+
+                              },100)
+
+        val sideAnimation1 = AnimationUtils.loadAnimation(this,R.anim.slide)
+        card2.startAnimation(sideAnimation)
+        Handler().postDelayed({
+                              },100)
 
 
 
